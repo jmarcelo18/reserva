b21bcc468dd7b18c60e9bbf6677dca8ee4f86bfa
jmarcelo18 <48532406+jmarcelo18@users.noreply.github.com>
2025-05-10 10:58:59 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  server: {
+    host: true, // Permite conexões de qualquer IP
+    port: 5173, // Porta padrão do Vite
+  },
+});
